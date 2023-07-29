e57b4638643d753956ff3e518a9cf8da71589e16
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-28 20:36:35 -0700

    completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', '1', 'M', 'brown', 'happy', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', '31', 'M', 'green', 'angry', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', '3', 'F', 'yellow', 'active', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', '21', 'M', 'red', 'sleepy', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', '3', 'M', 'purple', 'aggressive', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', '5', 'F', 'brown', 'happy', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', '6', 'M', 'brown', 'lively', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, '9', 'F', 'brown', 'active', '1');
